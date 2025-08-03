84d33d4afe99e89030634c53c8f2f89c4bbbeb45
addrianx <lastmarinebay@gmail.com>
2025-08-03 17:22:24 +0700

    fix asset

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,6 +1,6 @@
 <template>
     <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark"> <!--begin::Sidebar Brand-->
-        <div class="sidebar-brand"> <!--begin::Brand Link--> <a href="./index.html" class="brand-link"> <!--begin::Brand Image--> <img src="/dist/assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image opacity-75 shadow"> <!--end::Brand Image--> <!--begin::Brand Text--> <span class="brand-text fw-light">AdminLTE 4</span> <!--end::Brand Text--> </a> <!--end::Brand Link--> </div> <!--end::Sidebar Brand--> <!--begin::Sidebar Wrapper-->
+        <div class="sidebar-brand"> <!--begin::Brand Link--> <a href="" class="brand-link"> <!--begin::Brand Image--> <img src="/dist/assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image opacity-75 shadow"> <!--end::Brand Image--> <!--begin::Brand Text--> <span class="brand-text fw-light">AdminLTE 4</span> <!--end::Brand Text--> </a> <!--end::Brand Link--> </div> <!--end::Sidebar Brand--> <!--begin::Sidebar Wrapper-->
         <div class="sidebar-wrapper">
             <nav class="mt-2"> <!--begin::Sidebar Menu-->
                 <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
@@ -24,7 +24,7 @@
                             <li class="nav-item"> <router-link to="/dashboard/tambah-produk" class="nav-link" exact-active-class="active"> <i class="nav-icon bi bi-circle"></i>
                                     <p>Tambah</p>
                             </router-link> </li>
-                            <li class="nav-item"> <a href="./layout/collapsed-sidebar.html" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
+                            <li class="nav-item"> <a href="" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
                                     <p>Stok Item</p>
                                 </a> </li>
                             <li class="nav-item"> <router-link to="/dashboard/kategori-produk" class="nav-link" exact-active-class="active"> <i class="nav-icon bi bi-circle"></i>
@@ -39,10 +39,10 @@
                             </p>
                         </a>
                         <ul class="nav nav-treeview">
-                            <li class="nav-item"> <a href="./layout/unfixed-sidebar.html" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
+                            <li class="nav-item"> <a href="" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
                                     <p>Transaksi</p>
                                 </a> </li>
-                            <li class="nav-item"> <a href="./layout/fixed-sidebar.html" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
+                            <li class="nav-item"> <a href="" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
                                     <p>Transaksi Baru</p>
                                 </a> </li>
                         </ul>
@@ -54,10 +54,10 @@
                             </p>
                         </a>
                         <ul class="nav nav-treeview">
-                            <li class="nav-item"> <a href="./layout/unfixed-sidebar.html" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
+                            <li class="nav-item"> <a href="" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
                                     <p>Daftar Pelanggan</p>
                                 </a> </li>
-                            <li class="nav-item"> <a href="./layout/fixed-sidebar.html" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
+                            <li class="nav-item"> <a href="" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
                                     <p>Tambah Pelanggan</p>
                                 </a> </li>
                         </ul>
